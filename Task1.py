bec51d86ae27076dac087b7d1def4aec8afbb1ab
Wells.Yu <wells1022@gmail.com>
2022-10-14 00:44:18 +1100

    add comment

--- Task1.py
+++ Task1.py
@@ -1,11 +1,28 @@
+"""
+Programming Task #1
+Given a list of digits, determine the integer that it represents. 
+
+For example, given the list: [8,3,5,1], your program should output 8351.
+
+Note: You are not allowed to use conversion / casting functions, (i.e., str or int).
+
+"""
+
+
 def list_to_int(list_input):
+    """Convert the list of digits into integer and print out.
+
+    Args:
+        list of digits
+    """
     int_output = 0
     list_len = len(list_input)
     for num in list_input:
         list_len -= 1
+        # put the number in the correct digit
         int_output += num * pow(10, list_len)
     print(int_output)
 
 
-list1 = [8, 3, 5, 1]
-list_to_int(list1)
+list_input = [8, 3, 5, 1]
+list_to_int(list_input)
--- Task2.py
+++ Task2.py
@@ -1,10 +1,27 @@
+"""
+Programming Task #2
+Create a game where the user must correctly guess a randomly generated integer between 1 and 100.
+
+The user should be notified whether their guess was “lower” or “higher” than the target number.
+
+Note that you will need to use the random library’s randint function.
+
+Remember:
+- Use functions to group code
+- Try to have functions that only do one “thing” (e.g. print a message, validate user input, check what message to display)
+- Check for invalid inputs, notifying the user and re-prompting for a valid input
+
+"""
+
 import random
 
 
 def main():
+    """Main ligic of tha game
+        receive input and check result.
+    """
     print("Guess a number between 1 and 100: ")
     target_number = random.randint(1, 100)
-    print(target_number)
     while True:
         user_guess = input("Enter your guess: ")
         if not number_valid(user_guess):
@@ -14,6 +31,14 @@ def main():
 
 
 def number_valid(number):
+    """Check if the guess number is valid.
+
+    Args:
+        number (str): Guess from user
+
+    Returns:
+        bool: If guess is valid
+    """
     if number == "":
         print("Input can't be empty")
         return False
@@ -28,6 +53,15 @@ def number_valid(number):
 
 
 def check_answer(guess_int, target_number):
+    """Check if the guess meets the target.
+
+    Args:
+        guess_int (int): User guess
+        target_number (int): Target number
+
+    Returns:
+        bool: If guess meets the target
+    """
     if guess_int < target_number:
         print("Guess is lower than target!")
         return False
